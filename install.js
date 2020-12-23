98ecbf5d719415666dfba7a0b1d7e3b93af0ca8b
david-rojo <davidrojotech@gmail.com>
2020-12-23 01:21:15 +0100

    fix code alignment

--- install.js
+++ install.js
@@ -1,13 +1,10 @@
 const { spawnSync } = require('child_process');
 function exec(serviceName, command){
 
- console.log(`Installing dependencies for [${serviceName}]`);
- console.log(`Folder: ./${serviceName} Command: ${command}`);
+    console.log(`Installing dependencies for [${serviceName}]`);
+    console.log(`Folder: ./${serviceName} Command: ${command}`);
 
- spawnSync(
-    command,
-    [],
-    {
+    spawnSync(command, [], { 
         cwd: './' + serviceName,
         shell: true,
         stdio: 'inherit'
