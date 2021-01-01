bf17422131b3eb0e73769a7c9fe88d777ef84d02
david-rojo <davidrojotech@gmail.com>
2021-01-02 00:45:14 +0100

    planner grpc client for weather service implemented

--- planner/src/main/java/com/cloudapps/eoloplanner/planner/Application.java
+++ planner/src/main/java/com/cloudapps/eoloplanner/planner/Application.java
@@ -2,12 +2,14 @@ package com.cloudapps.eoloplanner.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class PlannerApplication {
+@EnableAsync
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlannerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ planner/src/main/java/com/cloudapps/eoloplanner/planner/service/WeatherService.java
@@ -0,0 +1,38 @@
+package com.cloudapps.eoloplanner.planner.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.cloudapps.eoloplanner.weatherservice.WeatherServiceGrpc.WeatherServiceBlockingStub;
+import com.cloudapps.eoloplanner.weatherservice.WeatherServiceOuterClass.GetWeatherRequest;
+import com.cloudapps.eoloplanner.weatherservice.WeatherServiceOuterClass.Weather;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+
+@Service
+public class WeatherService {
+
+	private Logger log = LoggerFactory.getLogger(WeatherService.class);
+	
+	@GrpcClient("weatherServer")
+	private WeatherServiceBlockingStub client;
+	
+	@Async
+    public CompletableFuture<String> getWeather(String city) {
+
+        GetWeatherRequest request = GetWeatherRequest.newBuilder()
+                .setCity(city)
+                .build();
+        
+        log.info("Executing weather request for city {}...", city);
+        Weather response = this.client.getWeather(request);
+        log.info("Weather response received for city {} is {}", city, response.getWeather());
+
+        return CompletableFuture.completedFuture(response.getWeather());
+    }
+	
+}
